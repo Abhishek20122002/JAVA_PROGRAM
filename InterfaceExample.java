035cf944e13929928efcdb10e0df276ab842818d
Abhishek20122002 <98834249+Abhishek20122002@users.noreply.github.com>
2023-03-16 21:50:25 +0530

    Add files via upload

---
+++ InterfaceExample.java
@@ -0,0 +1,18 @@
+import java.util.*;
+
+interface Example{
+    void print();
+}
+
+class Example1 implements Example{
+    @Override
+    public void print() {
+
+    }
+}
+
+public class InterfaceExample {
+    public static void main(String[] args) {
+
+    }
+}
