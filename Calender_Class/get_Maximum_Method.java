52921d9da1dd2c8b996d0d73f68bfc7a111fff55
Abhishek20122002 <98834249+Abhishek20122002@users.noreply.github.com>
2023-04-08 21:03:58 +0530

    Delete get_Maximum_Method.java

--- Calender_Class/get_Maximum_Method.java
+++
@@ -1,8 +0,0 @@
-package Calender_Class;
-import java.util.*;
-public class get_Maximum_Method {
-    public static void main(String[] args) {
-        Calendar c = Calendar.getInstance();
-        System.out.println("The maximum number of weeks in a year: "+c.getMaximum(Calendar.WEEK_OF_YEAR));
-    }
-}
