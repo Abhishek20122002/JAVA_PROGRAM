9265a59ee974a28642deb30a282cad731a5f149a
Abhishek20122002 <98834249+Abhishek20122002@users.noreply.github.com>
2023-04-09 00:22:01 +0530

    Add files via upload

---
+++ Calender_Class/Return_Week_Method.java
@@ -0,0 +1,9 @@
+package Calender_Class;
+import java.util.*;
+
+public class Return_Week_Method {
+    public static void main(String[] args) {
+        Calendar c = Calendar.getInstance();
+        System.out.println(c.getWeeksInWeekYear());
+    }
+}
